387ca7450fd0673a91f7081b29d5489669fe81bd
Ahsum1558 <abdullahcoxy1558@gmail.com>
2024-07-11 00:18:08 +0600

    Required Field Added to Visa Info

--- resources/views/admin/visa/visainfo/create.blade.php
+++ resources/views/admin/visa/visainfo/create.blade.php
@@ -25,15 +25,21 @@
                     <div class="profile-personal-info">
 
                         <div class="row mb-2">
+                            <div class="col">
+                                <h4 class="f-w-500 text-center">All <span class="text-danger font-weight-bold">(*)</span> fields must be completed</h4>
+                            </div>
+                        </div>
+
+                        <div class="row mb-2">
                             <div class="col-3">
-                                <h5 class="f-w-500">Visa No. in English<span class="pull-right">:</span></h5>
+                                <h5 class="f-w-500">Visa No. in English<span class="text-danger font-weight-bold">*</span><span class="pull-right">:</span></h5>
                             </div>
                             <div class="col-9"><input type="text" name="visano_en" class="form-control d-inline-block inline_setup" placeholder="Enter Visa No. in English" value="{{ old('visano_en') }}">
                             </div>
                         </div>
                         <div class="row mb-2">
                             <div class="col-3">
-                                <h5 class="f-w-500">Visa No. in Arabic<span class="pull-right">:</span></h5>
+                                <h5 class="f-w-500">Visa No. in Arabic<span class="text-danger font-weight-bold">*</span><span class="pull-right">:</span></h5>
                             </div>
                             <div class="col-9"><input type="text" name="visano_ar" class="form-control d-inline-block inline_setup" placeholder="Enter Visa No. in Arabic" value="{{ old('visano_ar') }}">
                             </div>
@@ -41,35 +47,35 @@
 
                         <div class="row mb-2">
                             <div class="col-3">
-                                <h5 class="f-w-500">Sponsor Id No. in English<span class="pull-right">:</span></h5>
+                                <h5 class="f-w-500">Sponsor Id No. in English<span class="text-danger font-weight-bold">*</span><span class="pull-right">:</span></h5>
                             </div>
                             <div class="col-9"><input type="text" name="sponsorid_en" class="form-control d-inline-block inline_setup" placeholder="Enter Sponsor Id No. in English" value="{{ old('sponsorid_en') }}">
                             </div>
                         </div>
                         <div class="row mb-2">
                             <div class="col-3">
-                                <h5 class="f-w-500">Sponsor Id No. in Arabic<span class="pull-right">:</span></h5>
+                                <h5 class="f-w-500">Sponsor Id No. in Arabic<span class="text-danger font-weight-bold">*</span><span class="pull-right">:</span></h5>
                             </div>
                             <div class="col-9"><input type="text" name="sponsorid_ar" class="form-control d-inline-block inline_setup" placeholder="Enter Sponsor Id No. in Arabic" value="{{ old('sponsorid_ar') }}">
                             </div>
                         </div>
                         <div class="row mb-2">
                             <div class="col-3">
-                                <h5 class="f-w-500">Sponsor Name in English<span class="pull-right">:</span></h5>
+                                <h5 class="f-w-500">Sponsor Name in English<span class="text-danger font-weight-bold">*</span><span class="pull-right">:</span></h5>
                             </div>
                             <div class="col-9"><input type="text" name="sponsorname_en" class="form-control d-inline-block inline_setup" placeholder="Enter Sponsor Name in English" value="{{ old('sponsorname_en') }}">
                             </div>
                         </div>
                         <div class="row mb-2">
                             <div class="col-3">
-                                <h5 class="f-w-500">Sponsor Name in Arabic<span class="pull-right">:</span></h5>
+                                <h5 class="f-w-500">Sponsor Name in Arabic<span class="text-danger font-weight-bold">*</span><span class="pull-right">:</span></h5>
                             </div>
                             <div class="col-9"><input type="text" name="sponsorname_ar" class="form-control d-inline-block inline_setup" placeholder="Enter Sponsor Name in Arabic" value="{{ old('sponsorname_ar') }}">
                             </div>
                         </div>
                         <div class="row mb-2">
                             <div class="col-3">
-                                <h5 class="f-w-500">Visa Date in Hijri<span class="pull-right">:</span></h5>
+                                <h5 class="f-w-500">Visa Date in Hijri<span class="text-danger font-weight-bold">*</span><span class="pull-right">:</span></h5>
                             </div>
                             <div class="col-9"><input type="text" name="visa_date" class="form-control d-inline-block inline_setup" placeholder="Enter Visa Date in Hijri" value="{{ old('visa_date') }}">
                             </div>
@@ -83,42 +89,42 @@
                         </div>
                         <div class="row mb-2">
                             <div class="col-3">
-                                <h5 class="f-w-500">Visa Occupation in English<span class="pull-right">:</span></h5>
+                                <h5 class="f-w-500">Visa Occupation in English<span class="text-danger font-weight-bold">*</span><span class="pull-right">:</span></h5>
                             </div>
                             <div class="col-9"><input type="text" name="occupation_en" class="form-control d-inline-block inline_setup" placeholder="Enter Visa Occupation in English" value="{{ old('occupation_en') }}">
                             </div>
                         </div>
                         <div class="row mb-2">
                             <div class="col-3">
-                                <h5 class="f-w-500">Visa Occupation in Arabic<span class="pull-right">:</span></h5>
+                                <h5 class="f-w-500">Visa Occupation in Arabic<span class="text-danger font-weight-bold">*</span><span class="pull-right">:</span></h5>
                             </div>
                             <div class="col-9"><input type="text" name="occupation_ar" class="form-control d-inline-block inline_setup" placeholder="Enter Visa Occupation in Arabic" value="{{ old('occupation_ar') }}">
                             </div>
                         </div>
                         <div class="row mb-2">
                             <div class="col-3">
-                                <h5 class="f-w-500">Visa Delegation/ Wakaalah No.<span class="pull-right">:</span></h5>
+                                <h5 class="f-w-500">Visa Delegation/ Wakaalah No.<span class="text-danger font-weight-bold">*</span><span class="pull-right">:</span></h5>
                             </div>
                             <div class="col-9"><input type="text" name="delegation_no" class="form-control d-inline-block inline_setup" placeholder="Enter Visa Delegation/ Wakaalah No." value="{{ old('delegation_no') }}">
                             </div>
                         </div>
                         <div class="row mb-2">
                             <div class="col-3">
-                                <h5 class="f-w-500">Visa Delegation/ Wakaalah Date<span class="pull-right">:</span></h5>
+                                <h5 class="f-w-500">Visa Delegation/ Wakaalah Date<span class="text-danger font-weight-bold">*</span><span class="pull-right">:</span></h5>
                             </div>
                             <div class="col-9"><input type="date" name="delegation_date" class="form-control d-inline-block inline_setup" value="{{ old('delegation_date') }}">
                             </div>
                         </div>
                         <div class="row mb-2">
                             <div class="col-3">
-                                <h5 class="f-w-500">Total Delegated Visa<span class="pull-right">:</span></h5>
+                                <h5 class="f-w-500">Total Delegated Visa<span class="text-danger font-weight-bold">*</span><span class="pull-right">:</span></h5>
                             </div>
                             <div class="col-9"><input type="text" name="delegated_visa" class="form-control d-inline-block inline_setup" placeholder="Enter Total Delegated Visa" value="{{ old('delegated_visa') }}">
                             </div>
                         </div>
                         <div class="row mb-2">
                             <div class="col-3">
-                                <h5 class="f-w-500">Contract Duration<span class="pull-right">:</span></h5>
+                                <h5 class="f-w-500">Contract Duration<span class="text-danger font-weight-bold">*</span><span class="pull-right">:</span></h5>
                             </div>
                             <div class="col-9"><input type="text" name="visa_duration" class="form-control d-inline-block inline_setup" placeholder="Enter Contract Duration" value="{{ old('visa_duration') }}">
                             </div>
@@ -126,7 +132,7 @@
 
                         <div class="row mb-2">
                             <div class="col-3">
-                                <h5 class="f-w-500">Status <span class="pull-right">:</span></h5>
+                                <h5 class="f-w-500">Status<span class="text-danger font-weight-bold">*</span><span class="pull-right">:</span></h5>
                             </div>
                             <div class="col-9">
                                 <select id="status" name="status" class="form-control d-inline-block inline_setup">
